2a3522e587310f02715e36da24a2ee39fd3a6fc5
Zero <node.zerocool@gmail.com>
2021-09-19 00:16:04 -0500

    design double func

---
+++ program_one.py
@@ -0,0 +1,18 @@
+numbers_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# procedural style
+doubled_list = []
+for x in numbers_list:
+  doubled_list.append(x * 2)
+
+print(doubled_list)
+
+#__
+
+# functional style
+
+def double(x):
+  return x * 2
+
+doubled_list_functional = list(map(double, numbers_list))
+print(doubled_list_functional)
